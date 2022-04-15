51d8f9e7ea4c5d38794c3ea8d9bb4f5123ed8b9e
Jason Schmitt <schmitty890@gmail.com>
2022-04-15 00:41:33 -1000

    fixing link

--- src/components/games/createGame.js
+++ src/components/games/createGame.js
@@ -4,7 +4,7 @@ export default function CreateGame() {
   return (
     <div className=" ">
       Create a game
-      <a href="createGame">
+      <a href="/createGame">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Create a game
         </button>

400b6847a592f2ab63748db7c66f3715290e2011
Jason Schmitt <schmitty890@gmail.com>
2022-05-11 11:59:14 -1000

    removing banner for now

--- src/components/headers/defaultHeader.js
+++ src/components/headers/defaultHeader.js
@@ -274,40 +274,6 @@ export default function DefaultHeader() {
               <Disclosure as="nav" className="bg-gray-800">
                 {({ open }) => (
                   <>
-                    {/* FOR STICKY BOTTOM */}
-                    {/* <div className="bg-yellow-600 fixed inset-x-0 bottom-0 p-4"> */}
-                    <div className="bg-yellow-600 ">
-                      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-                        <div className="flex items-center justify-between flex-wrap">
-                          <div className="w-0 flex-1 flex items-center">
-                            <span className="flex p-2 rounded-lg bg-red-700">
-                              <SpeakerphoneIcon
-                                className="h-6 w-6 text-white"
-                                aria-hidden="true"
-                              />
-                            </span>
-                            <p className="ml-3 font-medium text-white  ">
-                              <span className="md:hidden">
-                                BIG NEWS! We're launching our word guessing game
-                                site at the end of May!
-                              </span>
-                              <span className="hidden md:inline">
-                                BIG NEWS! We're launching our word guessing game
-                                site at the end of May!
-                              </span>
-                            </p>
-                          </div>
-                          {/* <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                      <a
-                        href="#"
-                        className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
-                      >
-                        Learn more
-                      </a>
-                    </div> */}
-                        </div>
-                      </div>
-                    </div>
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                       <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
